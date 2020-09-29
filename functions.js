c3ef5b2b48092b7a06d49a6ad056cd80f150175a
Mark Edward Ratio <markedwardratio@gmail.com>
2020-09-29 13:09:38 +0800

    Add capitalize function and its tests

---
+++ functions.js
@@ -0,0 +1,23 @@
+const capitalize = (str) => {
+	return str
+		.split(' ')
+		.map((word) => {
+			const firstLetterArr = word.match(/[a-zA-Z]/);
+			if (!firstLetterArr) {
+				return word;
+			}
+			const firstLetter = firstLetterArr[0];
+			const firstLetterIndex = word.indexOf(firstLetter);
+			const wordArr = word.split('').map((char, index) => {
+				return index === firstLetterIndex
+					? char.toUpperCase()
+					: char.toLowerCase();
+			});
+			return wordArr.join('');
+		})
+		.join(' ');
+};
+
+module.exports = {
+	capitalize,
+};
---
+++ functions.test.js
@@ -0,0 +1,49 @@
+const { capitalize } = require('./functions');
+
+// capitalize test
+
+test('capitalize a letter', () => {
+	const str = capitalize('a');
+	expect(str).toBe('A');
+});
+
+test('capitalize a word', () => {
+	const str = capitalize('hello');
+	expect(str).toBe('Hello');
+});
+
+test('capitalize a word with mixed letter case', () => {
+	const str = capitalize('hElLo');
+	expect(str).toBe('Hello');
+});
+
+test('capitalize multiple words', () => {
+	const str = capitalize('this is a sentence');
+	expect(str).toBe('This Is A Sentence');
+});
+
+test('capitalize multiple words with mixed letter case', () => {
+	const str = capitalize('tHiS iS A seNTeNcE');
+	expect(str).toBe('This Is A Sentence');
+});
+
+test('capitalize multiple words with punctuations', () => {
+	const str = capitalize(
+		'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+	);
+	expect(str).toBe('Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.');
+});
+
+test('capitalize multiple words with punctuation at the beggining of a word', () => {
+	const str = capitalize(
+		'"lorem ipsum dolor sit amet, consectetur adipiscing elit."'
+	);
+	expect(str).toBe(
+		'"Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit."'
+	);
+});
+
+test('accepts nonletter', () => {
+	const str = capitalize('the clothing costs $30.');
+	expect(str).toBe('The Clothing Costs $30.');
+});
